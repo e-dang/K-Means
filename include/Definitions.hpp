d297851d80c97150c456265be55f94fe429c67a9
Eric Dang <edang830@gmail.com>
2019-11-07 17:35:56 -0800

    add header file for typedefs

---
+++ include/Definitions.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+
+typedef float value_t;
+typedef std::vector<value_t> datapoint_t;
+typedef std::vector<datapoint_t> dataset_t;
+typedef std::vector<int> clustering_t;
+typedef struct
+{
+    int count;
+    datapoint_t coords;
+} cluster_t;
