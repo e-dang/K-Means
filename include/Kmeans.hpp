7e1ddcbeac80026a6d24b18a6f8f5260b9adf8f2
Eric Dang <edang830@gmail.com>
2019-11-07 17:41:52 -0800

    add getter and setter methods

--- include/Kmeans.hpp
+++ include/Kmeans.hpp
@@ -9,13 +9,13 @@ private:
      * @brief Private member variables
      *
      */
-    int numClusters;              // the number of clusters to cluster to data into
-    int numRestarts;              // the number of times Kmeans should try to cluster the data
-    float finalError;             // the error in the final clustering
-    cluster_t clusters;           // the cluster centers
-    cluster_t finalClusters;      // the final cluster centers
-    clustering_t clustering;      // the cluster assignments for each data point
-    clustering_t finalClustering; // the final cluster assignment
+    int numClusters;             // the number of clusters to cluster to data into
+    int numRestarts;             // the number of times Kmeans should try to cluster the data
+    float bestError;             // the error in the best clustering
+    cluster_t clusters;          // the cluster centers
+    cluster_t bestClusters;      // the best cluster centers
+    clustering_t clustering;     // the cluster assignments for each data point
+    clustering_t bestClustering; // the best cluster assignments
 
 public:
     /**
@@ -31,4 +31,60 @@ public:
      *
      */
     ~Kmeans();
+
+    /**
+     * @brief Get the numClusters object.
+     *
+     * @return int
+     */
+    int getNumClusters()
+    {
+        return numClusters;
+    }
+
+    /**
+     * @brief Get the numRestarts object.
+     *
+     * @return int
+     */
+    int getNumRestarts() { return numRestarts; }
+
+    /**
+     * @brief Get the bestClustering object.
+     *
+     * @return clusteringPtr_t
+     */
+    clustering_t getClustering() { return bestClustering; }
+
+    /**
+     * @brief Get the bestClusters object.
+     *
+     * @return clustersPtr_t
+     */
+    cluster_t getClusters() { return bestClusters; }
+
+    /**
+     * @brief Get the bestError.
+     *
+     * @return int
+     */
+    float getError() { return bestError; };
+
+    /**
+     * @brief Set the numClusters object.
+     *
+     * @param numClusters
+     * @return true
+     * @return false
+     */
+    bool setNumClusters(int numClusters);
+
+    /**
+     * @brief Set the numRestarts object.
+     *
+     * @param numRestarts
+     * @return true
+     * @return false
+     */
+    bool setNumRestarts(int numRestarts);
 };
--- src/Kmeans.cpp
+++ src/Kmeans.cpp
@@ -2,9 +2,21 @@
 
 Kmeans::Kmeans(int numClusters, int numRestarts) : numClusters(numClusters), numRestarts(numRestarts)
 {
-    finalError = INT_MAX;
+    bestError = INT_MAX;
 }
 
 Kmeans::~Kmeans()
 {
+}
+
+bool Kmeans::setNumClusters(int numClusters)
+{
+    this->numClusters = numClusters;
+    return true;
+}
+
+bool Kmeans::setNumRestarts(int numRestarts)
+{
+    this->numRestarts = numRestarts;
+    return true;
 }
