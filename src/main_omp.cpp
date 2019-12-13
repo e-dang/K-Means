9c0380171f7581f25f97181be70641052ea12d60
Eric Dang <edang830@gmail.com>
2019-12-13 13:39:12 -0800

    update

--- src/main_omp.cpp
+++ src/main_omp.cpp
@@ -5,20 +5,23 @@
 #include <chrono>
 #include "Kmeans.hpp"
 #include <vector>
+// #include "omp.h"
 
 int main(int argc, char *argv[])
 {
     int numRestarts = 10;
-    int numData = 1000000;
+    int numData = 100000;
     int numFeatures = 2;
-    int numClusters = 10000;
+    int numClusters = 30;
     int initIters = 4;
-    float overSampling = numClusters / initIters;
+    float overSampling = numClusters / 2;
+    int numThreads = 8;
 
-    std::vector<float> times;
+    std::vector<double> times;
 
     DataSetReader reader;
-    reader.read("data_1000000_2.txt", numData, numFeatures);
+    std::string inFile = "../test_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + ".txt";
+    reader.read(inFile, numData, numFeatures);
     dataset_t data = reader.getData();
 
     RNGType rng(time(NULL));
@@ -32,46 +35,67 @@ int main(int argc, char *argv[])
     auto start = std::chrono::high_resolution_clock::now();
     serialKmeanskpp.fit(data, Kmeans::distanceL2);
     auto stop = std::chrono::high_resolution_clock::now();
-    auto durationSerialKPP = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    auto durationSerialKPP = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     times.push_back(durationSerialKPP.count());
     ClusterWriter writerKpp(serialKmeanskpp.getClusters(), serialKmeanskpp.getClustering());
-    writerKpp.writeClusters("../clusters_serial_kpp.txt");
-    writerKpp.writeClustering("../clustering_serial_kpp.txt");
+    std::string serialKppClustersFile = "../clusters_serial_kpp_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + ".txt";
+    std::string serialKppClusteringFile = "../clustering_serial_kpp_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + ".txt";
+    writerKpp.writeClusters(serialKppClustersFile);
+    writerKpp.writeClustering(serialKppClusteringFile);
+
+    std::cout << "Serial KPlusPlus Done!" << std::endl;
 
     // Serial Kmeans Scaleable
     SerialKmeans serialKmeansScale(numClusters, numRestarts, intDistr, floatDistr);
-    auto start = std::chrono::high_resolution_clock::now();
+    start = std::chrono::high_resolution_clock::now();
     serialKmeansScale.fit(data, overSampling, Kmeans::distanceL2, initIters);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto durationSerialScaleable = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    stop = std::chrono::high_resolution_clock::now();
+    auto durationSerialScaleable = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     times.push_back(durationSerialScaleable.count());
     ClusterWriter writerScale(serialKmeansScale.getClusters(), serialKmeansScale.getClustering());
-    writerScale.writeClusters("../clusters_serial_scale.txt");
-    writerScale.writeClustering("../clustering_serial_scale.txt");
+    std::string serialScaleClustersFile = "../clusters_serial_scale_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + ".txt";
+    std::string serialScaleClusteringFile = "../clustering_serial_scale_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + ".txt";
+    writerScale.writeClusters(serialScaleClustersFile);
+    writerScale.writeClustering(serialScaleClusteringFile);
+
+    std::cout << "Serial Scaleable Done!" << std::endl;
 
     // Omp Kmeans KPlusPlus
-    OMPKmeans ompKmeanskpp(numClusters, numClusters, intDistr, floatDistr);
-    auto start = std::chrono::high_resolution_clock::now();
+    // #pragma omp parallel
+    //     {
+    //         std::cout << omp_get_num_threads() << std::endl;
+    //     }
+
+    OMPKmeans ompKmeanskpp(numClusters, numRestarts, intDistr, floatDistr, numThreads);
+    start = std::chrono::high_resolution_clock::now();
     ompKmeanskpp.fit(data, Kmeans::distanceL2);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto durationOmpKPP = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    stop = std::chrono::high_resolution_clock::now();
+    auto durationOmpKPP = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     times.push_back(durationOmpKPP.count());
     ClusterWriter writerOmpKpp(ompKmeanskpp.getClusters(), ompKmeanskpp.getClustering());
-    writerOmpKpp.writeClusters("../clusters_omp_kpp.txt");
-    writerOmpKpp.writeClustering("../clustering_omp_kpp.txt");
+    std::string ompKppClustersFile = "../clusters_omp_kpp_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + "_" + std::to_string(numThreads) + ".txt";
+    std::string ompKppClusteringFile = "../clustering_omp_kpp_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + "_" + std::to_string(numThreads) + ".txt";
+    writerOmpKpp.writeClusters(ompKppClustersFile);
+    writerOmpKpp.writeClustering(ompKppClusteringFile);
+
+    std::cout << "OMP KPlusPlus Done!" << std::endl;
 
     // Omp Kmeans Scaleable
-    OMPKmeans ompKmeansScale(numClusters, numClusters, intDistr, floatDistr);
-    auto start = std::chrono::high_resolution_clock::now();
+    OMPKmeans ompKmeansScale(numClusters, numRestarts, intDistr, floatDistr, numThreads);
+    start = std::chrono::high_resolution_clock::now();
     ompKmeansScale.fit(data, overSampling, Kmeans::distanceL2, initIters);
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto durationOmpScale = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    stop = std::chrono::high_resolution_clock::now();
+    auto durationOmpScale = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
     times.push_back(durationOmpScale.count());
     ClusterWriter writerOmpScale(ompKmeansScale.getClusters(), ompKmeansScale.getClustering());
-    writerOmpScale.writeClusters("../clusters_omp_scale.txt");
-    writerOmpScale.writeClustering("../clustering_omp_scale.txt");
+    std::string ompScaleClustersFile = "../clusters_omp_scale_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + "_" + std::to_string(numThreads) + ".txt";
+    std::string ompScaleClusteringFile = "../clustering_omp_scale_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + "_" + std::to_string(numThreads) + ".txt";
+    writerOmpScale.writeClusters(ompScaleClustersFile);
+    writerOmpScale.writeClustering(ompScaleClusteringFile);
 
-    writerOmpScale.writeTimes(times, "../times.txt");
+    std::string timesFile = "../times_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + "_serial.txt";
+    writerScale.writeTimes(times, timesFile);
 
+    // std::cout << "OMP Scaleable Done!" << std::endl;
     exit(0);
 }
