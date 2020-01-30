b0b60e233a2b1dcb70e6bd12b1618859f8e7ee80
Eric Dang <edang830@gmail.com>
2020-01-30 15:45:24 -0800

    remove unused definitions

--- include/Definitions.hpp
+++ include/Definitions.hpp
@@ -4,31 +4,6 @@
 
 typedef float value_t;
 
-struct MPIDataChunks
-{
-    int rank;
-    Matrix matrixChunk;
-    std::vector<int> lengths;
-    std::vector<int> displacements;
-};
-
-struct BundledAlgorithmData
-{
-    Matrix *matrix;
-    std::vector<value_t> *weights;
-
-    BundledAlgorithmData(Matrix *matrix, std::vector<value_t> *weights) : matrix(matrix), weights(weights){};
-    virtual ~BundledAlgorithmData(){};
-};
-
-struct BundledMPIAlgorithmData : public BundledAlgorithmData
-{
-    MPIDataChunks *dataChunks;
-
-    BundledMPIAlgorithmData(Matrix *matrix, std::vector<value_t> *weights, MPIDataChunks *dataChunks) : BundledAlgorithmData(matrix, weights), dataChunks(dataChunks){};
-    ~BundledMPIAlgorithmData(){};
-};
-
 typedef std::vector<value_t> datapoint_t;
 typedef std::vector<datapoint_t> dataset_t;
 typedef std::vector<int> clustering_t;
