e0808eede0dff03e386ad7ce6da36a7119836787
Eric Dang <edang830@gmail.com>
2020-01-30 00:48:02 -0800

    implement ClusteringUpdater classes

---
+++ include/ClusteringUpdater.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Definitions.hpp"
+
+class AbstractClusteringUpdater
+{
+protected:
+    std::vector<int> **ppClustering;
+    std::vector<value_t> **ppClusterWeights;
+
+public:
+    AbstractClusteringUpdater(std::vector<int> *clustering, std::vector<value_t> *clusterWeights) : ppClustering(&clustering),
+                                                                                                    ppClusterWeights(&clusterWeights) {}
+
+    virtual void update(const int &dataIdx, const int &clusterIdx, const value_t &weight) = 0;
+};
+
+class ClusteringUpdater : public AbstractClusteringUpdater
+{
+public:
+    ClusteringUpdater(std::vector<int> *clustering, std::vector<value_t> *clusterWeights) : AbstractClusteringUpdater(clustering, clusterWeights) {}
+
+    void update(const int &dataIdx, const int &clusterIdx, const value_t &weight) override;
+};
+
+class AtomicClusteringUpdater : public AbstractClusteringUpdater
+{
+public:
+    AtomicClusteringUpdater(std::vector<int> *clustering, std::vector<value_t> *clusterWeights) : AbstractClusteringUpdater(clustering, clusterWeights) {}
+
+    void update(const int &dataIdx, const int &clusterIdx, const value_t &weight) override;
+};
+
+class DistributedClusteringUpdater : public AbstractClusteringUpdater
+{
+public:
+    DistributedClusteringUpdater(std::vector<int> *clustering, std::vector<value_t> *clusterWeights) : AbstractClusteringUpdater(clustering, clusterWeights) {}
+
+    void update(const int &dataIdx, const int &clusterIdx, const value_t &weight) override;
+};
---
+++ src/ClusteringUpdater.cpp
@@ -0,0 +1,48 @@
+#include "ClusteringUpdater.hpp"
+
+void ClusteringUpdater::update(const int &dataIdx, const int &clusterIdx, const value_t &weight)
+{
+    int &clusterAssignment = (*ppClustering)->at(dataIdx);
+
+    // only go through this update if the cluster assignment is going to change
+    if (clusterAssignment != clusterIdx)
+    {
+        // cluster assignments are initialized to -1, so ignore decrement if datapoint has yet to be assigned
+        if (clusterAssignment >= 0 && (*ppClusterWeights)->at(clusterAssignment) > 0)
+            (*ppClusterWeights)->at(clusterAssignment) -= weight;
+        (*ppClusterWeights)->at(clusterIdx) += weight;
+        clusterAssignment = clusterIdx;
+    }
+}
+
+void AtomicClusteringUpdater::update(const int &dataIdx, const int &clusterIdx, const value_t &weight)
+{
+    int &clusterAssignment = (*ppClustering)->at(dataIdx);
+
+    // only go through this update if the cluster assignment is going to change
+    if (clusterAssignment != clusterIdx)
+    {
+        // cluster assignments are initialized to -1, so ignore decrement if datapoint has yet to be assigned
+        if (clusterAssignment >= 0 && (*ppClusterWeights)->at(clusterAssignment) > 0)
+#pragma omp atomic
+            (*ppClusterWeights)->at(clusterAssignment) -= weight;
+#pragma omp atomic
+        (*ppClusterWeights)->at(clusterIdx) += weight;
+        clusterAssignment = clusterIdx;
+    }
+}
+
+void DistributedClusteringUpdater::update(const int &dataIdx, const int &clusterIdx, const value_t &weight)
+{
+    int &clusterAssignment = (*ppClustering)->at(dataIdx);
+
+    // only go through this update if the cluster assignment is going to change
+    if (clusterAssignment != clusterIdx)
+    {
+        // cluster assignments are initialized to -1, so ignore decrement if datapoint has yet to be assigned
+        if (clusterAssignment >= 0)
+            (*ppClusterWeights)->at(clusterAssignment) -= weight;
+        (*ppClusterWeights)->at(clusterIdx) += weight;
+        clusterAssignment = clusterIdx;
+    }
+}
