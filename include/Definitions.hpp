8cda8797b5a789c362624a3d733c0ad58132d3ab
Eric Dang <edang830@gmail.com>
2020-02-02 00:17:21 -0800

    remove unused data definitions

--- include/Definitions.hpp
+++ include/Definitions.hpp
@@ -1,23 +1,3 @@
 #pragma once
 
-#include <vector>
-
 typedef float value_t;
-
-typedef std::vector<value_t> datapoint_t;
-typedef std::vector<datapoint_t> dataset_t;
-typedef std::vector<int> clustering_t;
-
-typedef struct
-{
-    dataset_t data;
-    std::vector<value_t> weights;
-} coreset_t;
-
-typedef struct
-{
-    int count;
-    datapoint_t coords;
-} cluster_t;
-
-typedef std::vector<cluster_t> clusters_t;
