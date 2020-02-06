61274273540773ab1b7bab7d8b35a6f752696113
Eric Dang <edang830@gmail.com>
2020-02-06 01:56:50 -0800

    Remove unused function

--- include/KmeansDataCreator.hpp
+++ include/KmeansDataCreator.hpp
@@ -9,8 +9,6 @@ class IKmeansDataCreator
 public:
     virtual KmeansData create(Matrix* data, std::vector<value_t>* weights,
                               std::shared_ptr<IDistanceFunctor> distanceFunc) = 0;
-
-    virtual unsigned long getTotalNumData(const Matrix* const data) = 0;
 };
 
 class SharedMemoryKmeansDataCreator : public IKmeansDataCreator
@@ -22,8 +20,6 @@ public:
         return KmeansData(data, weights, distanceFunc, 0, data->getNumData(), std::vector<int>(1, data->getNumData()),
                           std::vector<int>(1, 0));
     }
-
-    virtual unsigned long getTotalNumData(const Matrix* const data) { return data->getNumData(); }
 };
 
 class MPIKmeansDataCreator : public IKmeansDataCreator
@@ -32,11 +28,9 @@ public:
     KmeansData create(Matrix* data, std::vector<value_t>* weights,
                       std::shared_ptr<IDistanceFunctor> distanceFunc) override
     {
-        auto totalNumData = getTotalNumData(data);
+        auto totalNumData = getTotalNumDataMPI(data);
         auto mpiData      = getMPIData(totalNumData);
         return KmeansData(data, weights, distanceFunc, mpiData.rank, totalNumData, mpiData.lengths,
                           mpiData.displacements);
     }
-
-    virtual unsigned long getTotalNumData(const Matrix* const data) { return getTotalNumDataMPI(data); }
 };
