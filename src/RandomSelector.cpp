40192e89388927744fc648f0db5f01ff7863f44f
Eric Dang <edang830@gmail.com>
2020-02-06 23:38:22 -0800

    Change types

--- src/RandomSelector.cpp
+++ src/RandomSelector.cpp
@@ -2,12 +2,9 @@
 
 #include "Utils.hpp"
 
-int SingleWeightedRandomSelector::select(const std::vector<value_t>* const weights, value_t randomSumFrac)
+int_fast32_t SingleWeightedRandomSelector::select(const std::vector<value_t>* const weights, value_t randomSumFrac)
 {
-    int maxIdx = weights->size();
-
-    // each iteration substract the weight from the cutoff and once it reaches <= 0, the corresponding index is selected
-    for (int i = 0; i < maxIdx; i++)
+    for (size_t i = 0; i < weights->size(); i++)
     {
         if ((randomSumFrac -= weights->at(i)) <= 0)
         {
@@ -15,30 +12,31 @@ int SingleWeightedRandomSelector::select(const std::vector<value_t>* const weigh
         }
     }
 
-    return maxIdx - 1;
+    return static_cast<int_fast32_t>(weights->size()) - 1;
 }
 
-std::vector<int> MultiWeightedRandomSelector::select(const std::vector<value_t>* const weights,
-                                                     const int_fast32_t& sampleSize)
+std::vector<int_fast32_t> MultiWeightedRandomSelector::select(const std::vector<value_t>* const weights,
+                                                              const int_fast32_t& sampleSize)
 {
     std::vector<double> vals;
-    for (auto& val : *weights)
+    for (const auto& val : *weights)
     {
         vals.push_back(std::pow(getRandDouble01(), 1.0 / val));
     }
 
-    std::vector<std::pair<int, value_t>> valsWithIndices;
-    for (int i = 0; i < vals.size(); i++)
+    std::vector<std::pair<int_fast32_t, value_t>> valsWithIndices;
+    for (size_t i = 0; i < vals.size(); i++)
     {
         valsWithIndices.emplace_back(i, vals[i]);
     }
-    std::sort(valsWithIndices.begin(), valsWithIndices.end(),
-              [](std::pair<int, value_t> x, std::pair<int, value_t> y) { return x.second > y.second; });
+    std::sort(
+      valsWithIndices.begin(), valsWithIndices.end(),
+      [](std::pair<int_fast32_t, value_t> x, std::pair<int_fast32_t, value_t> y) { return x.second > y.second; });
 
-    std::vector<int> samples;
+    std::vector<int_fast32_t> samples;
     for (int_fast32_t i = 0; i < sampleSize; i++)
     {
-        samples.push_back(valsWithIndices[i].first);
+        samples.emplace_back(valsWithIndices[i].first);
     }
 
     return samples;
