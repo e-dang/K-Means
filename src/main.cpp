80da47fbf9f6f2e33cabf8484ced1f87687fcbd9
Eric Dang <edang830@gmail.com>
2020-02-07 01:38:17 -0800

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,11 @@
 
 int main(int argc, char* argv[])
 {
-    int numRestarts = 50;
-    int numData     = 100000;
-    int numFeatures = 2;
-    int numClusters = 30;
-    int sampleSize  = 5000;
+    int numRestarts          = 50;
+    int_fast32_t numData     = 100000;
+    int_fast32_t numFeatures = 2;
+    int_fast32_t numClusters = 30;
+    int_fast32_t sampleSize  = 5000;
 
     int rank = 0, numProcs;
     MPI_Init(&argc, &argv);
@@ -38,6 +38,24 @@ int main(int argc, char* argv[])
     Matrix matrix(reader.getData(), numData / numProcs, numFeatures);
     // matrix.display();
 
+    // Kmeans kmeans(KPP, Lloyd, None, Serial, std::make_shared<EuclideanDistance>());
+    // Kmeans kmeans(OptKPP, OptLloyd, None, Serial, std::make_shared<EuclideanDistance>());
+    // Kmeans kmeans(KPP, Lloyd, LWCoreset, Serial, std::make_shared<EuclideanDistance>(), sampleSize);
+    // Kmeans kmeans(OptKPP, OptLloyd, LWCoreset, Serial, std::make_shared<EuclideanDistance>(), sampleSize);
+
+    // Kmeans kmeans(KPP, Lloyd, None, OMP, std::make_shared<EuclideanDistance>());
+    // Kmeans kmeans(OptKPP, OptLloyd, None, OMP, std::make_shared<EuclideanDistance>());
+    // Kmeans kmeans(KPP, Lloyd, LWCoreset, OMP, std::make_shared<EuclideanDistance>(), sampleSize);
+    // Kmeans kmeans(OptKPP, OptLloyd, LWCoreset, OMP, std::make_shared<EuclideanDistance>(), sampleSize);
+
+    // Kmeans kmeans(KPP, Lloyd, None, MPI, std::make_shared<EuclideanDistance>());
+    // Kmeans kmeans(OptKPP, OptLloyd, None, MPI, std::make_shared<EuclideanDistance>());
+    // Kmeans kmeans(KPP, Lloyd, LWCoreset, MPI, std::make_shared<EuclideanDistance>(), sampleSize);
+    // Kmeans kmeans(OptKPP, OptLloyd, LWCoreset, MPI, std::make_shared<EuclideanDistance>(), sampleSize);
+
+    // Kmeans kmeans(KPP, Lloyd, None, Hybrid, std::make_shared<EuclideanDistance>(), sampleSize);
+    // Kmeans kmeans(OptKPP, OptLloyd, None, Hybrid, std::make_shared<EuclideanDistance>(), sampleSize);
+    // Kmeans kmeans(KPP, Lloyd, LWCoreset, Hybrid, std::make_shared<EuclideanDistance>(), sampleSize);
     Kmeans kmeans(OptKPP, OptLloyd, LWCoreset, Hybrid, std::make_shared<EuclideanDistance>(), sampleSize);
 
     auto start             = std::chrono::high_resolution_clock::now();
@@ -47,7 +65,7 @@ int main(int argc, char* argv[])
 
     if (rank == 0)
     {
-        ClusterDataWriter writer(clusterResults.mClusterData, numData, numFeatures);
+        ClusterDataWriter writer(clusterResults->mClusterData, numData, numFeatures);
         std::string serialKppClustersFile =
           "../data/clusters_serial_kpp_" + std::to_string(numData) + "_" + std::to_string(numFeatures) + ".txt";
         std::string serialKppClusteringFile =
@@ -55,9 +73,9 @@ int main(int argc, char* argv[])
         writer.writeClusters(serialKppClustersFile);
         writer.writeClustering(serialKppClusteringFile);
 
-        std::cout << "Serial KPlusPlus Done! Error: " << clusterResults.mError << " Time: " << durationSerialKPP.count()
-                  << std::endl;
-        auto clusterdata = clusterResults.mClusterData;
+        std::cout << "Serial KPlusPlus Done! Error: " << clusterResults->mError
+                  << " Time: " << durationSerialKPP.count() << std::endl;
+        auto clusterdata = clusterResults->mClusterData;
         // std::cout << "Clustering" << std::endl;
         // int count = 0;
         // for (auto &val : clusterdata.mClustering)
