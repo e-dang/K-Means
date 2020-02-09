50e11d898d309dd332862712483fedd4d84dabaa
Eric Dang <edang830@gmail.com>
2020-02-09 02:13:45 -0800

    Add default enum values

--- include/Containers/DataClasses.hpp
+++ include/Containers/DataClasses.hpp
@@ -134,28 +134,31 @@ struct MPIData
 
 enum Initializer
 {
-    KPP    = 1 << 0,
-    OptKPP = 1 << 1
+    InitNull = 0,
+    KPP      = 1 << 0,
+    OptKPP   = 1 << 1
 };
 
 enum Maximizer
 {
+    MaxNull  = 0,
     Lloyd    = 1 << 2,
     OptLloyd = 1 << 3
 };
 
 enum CoresetCreator
 {
+    None      = 0,
     LWCoreset = 1 << 4,
-    None      = 0
 };
 
 enum Parallelism
 {
-    Serial = 1 << 5,
-    OMP    = 1 << 6,
-    MPI    = 1 << 7,
-    Hybrid = 1 << 8
+    ParaNull = 0,
+    Serial   = 1 << 5,
+    OMP      = 1 << 6,
+    MPI      = 1 << 7,
+    Hybrid   = 1 << 8
 };
 
 enum Variant
