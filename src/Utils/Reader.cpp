97d331947326413d4f6fee00c2541d23cf7ace68
Eric Dang <edang830@gmail.com>
2020-02-10 22:26:26 -0800

    Fix bug where incorrect number of data was read

--- src/Utils/Reader.cpp
+++ src/Utils/Reader.cpp
@@ -24,22 +24,26 @@ std::vector<value_t> VectorReader::read(const std::string& filepath, const int32
 std::vector<value_t> MPIReader::read(const std::string& filepath, const int32_t& numData, const int32_t& numFeatures)
 {
     auto mpiData      = getMPIData(numData);
-    MPI_Offset offset = mpiData.displacements[mpiData.rank];
+    auto lengths      = mpiData.lengths[mpiData.rank] * numFeatures;
+    MPI_Offset offset = mpiData.displacements[mpiData.rank] * numFeatures * sizeof(value_t);
     MPI_File fh;
     MPI_Status status;
 
     MPI_File_open(MPI_COMM_WORLD, filepath.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
 
-    std::vector<value_t> data(mpiData.lengths[mpiData.rank]);
-    MPI_File_read_at(fh, offset, data.data(), mpiData.lengths[mpiData.rank], mpi_type_t, &status);
+    std::vector<value_t> data(lengths);
+    MPI_File_read_at(fh, offset, data.data(), lengths, mpi_type_t, &status);
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_File_close(&fh);
 
-    if (data.size() == 0)
+    int count;
+    MPI_Get_count(&status, MPI_INT, &count);
+    MPI_Allreduce(MPI_IN_PLACE, &count, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    if (numData * numFeatures != count / (sizeof(value_t) / sizeof(float)) || count < 0)
     {
         if (mpiData.rank == 0)
-            throw std::runtime_error("Zero data points were read in from the given file.");
-
+            std::cerr << count / mpiData.numProcs << " data points were read in from the given file, but " << numData
+                      << " data points were specified." << std::endl;
         exit(1);
     }
 
