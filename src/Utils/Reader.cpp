7353d847f59f9a8602e884a86c726973dfb3d65a
Eric Dang <edang830@gmail.com>
2020-02-10 19:55:56 -0800

    Fix bug with process reading lengths

--- src/Utils/Reader.cpp
+++ src/Utils/Reader.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <iostream>
 
+#include "Utils/Utils.hpp"
 #include "mpi.h"
 
 std::vector<value_t> VectorReader::read(const std::string& filepath, const int32_t& numData, const int32_t& numFeatures)
@@ -22,25 +23,21 @@ std::vector<value_t> VectorReader::read(const std::string& filepath, const int32
 
 std::vector<value_t> MPIReader::read(const std::string& filepath, const int32_t& numData, const int32_t& numFeatures)
 {
-    int rank, numProcs;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
-
-    int32_t numDataPerProc = numData * numFeatures / numProcs;
-    MPI_Offset offset      = rank * numDataPerProc * sizeof(value_t);
+    auto mpiData      = getMPIData(numData);
+    MPI_Offset offset = mpiData.displacements[mpiData.rank];
     MPI_File fh;
     MPI_Status status;
 
     MPI_File_open(MPI_COMM_WORLD, filepath.c_str(), MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
 
-    std::vector<value_t> data(numDataPerProc);
-    MPI_File_read_at(fh, offset, data.data(), numDataPerProc, mpi_type_t, &status);
+    std::vector<value_t> data(mpiData.lengths[mpiData.rank]);
+    MPI_File_read_at(fh, offset, data.data(), mpiData.lengths[mpiData.rank], mpi_type_t, &status);
     MPI_Barrier(MPI_COMM_WORLD);
     MPI_File_close(&fh);
 
     if (data.size() == 0)
     {
-        if (rank == 0)
+        if (mpiData.rank == 0)
             throw std::runtime_error("Zero data points were read in from the given file.");
 
         exit(1);
