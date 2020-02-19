c50a2bebc1478b59e19285f4b264fd2324fca754
Eric Dang <edang830@gmail.com>
2020-02-19 00:50:00 -0800

    Modify KmeansState API and functionality

--- include/Containers/KmeansState.hpp
+++ include/Containers/KmeansState.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <numeric>
+
 #include "Containers/DataClasses.hpp"
 #include "Containers/Matrix.hpp"
 
@@ -20,10 +22,10 @@ private:
     const std::shared_ptr<IDistanceFunctor<precision>> p_DistanceFunc;
 
     // dynamic data that changes each repeat
-    Matrix<precision, int_size>* p_Clusters;
-    std::vector<int_size>* p_Clustering;
-    std::vector<precision>* p_ClusterWeights;
-    std::vector<precision>* p_SqDistances;
+    std::shared_ptr<Matrix<precision, int_size>> p_Clusters;
+    std::shared_ptr<std::vector<int_size>> p_Clustering;
+    std::shared_ptr<std::vector<precision>> p_ClusterWeights;
+    std::shared_ptr<std::vector<precision>> p_SqDistances;
 
 public:
     KmeansState(const Matrix<precision, int_size>* const data, const std::vector<precision>* const weights,
@@ -37,35 +39,73 @@ public:
         m_Displacement(displacements.at(rank)),
         p_Data(data),
         p_Weights(weights),
-        p_DistanceFunc(distanceFunc)
+        p_DistanceFunc(distanceFunc),
+        p_Clusters(nullptr),
+        p_Clustering(nullptr),
+        p_ClusterWeights(nullptr),
+        p_SqDistances(nullptr)
     {
     }
 
     ~KmeansState() = default;
 
-    inline precision operator()(const precision* point1, const precision* point2, const int32_t& numFeatures)
+    inline precision operator()(const precision* point1, const precision* point2)
     {
-        return (*p_DistanceFunc)(point1, point2, numFeatures);
+        return (*p_DistanceFunc)(point1, point2, dataCols());
     }
 
-    void setClusterData(ClusterData<precision, int_size>* const clusterData)
+    inline void resetClusterData(const int_size& numClusters, const precision& sqDistsFillVal = -1.0)
     {
-        p_Clusters       = &clusterData->clusters;
-        p_Clustering     = &clusterData->clustering;
-        p_ClusterWeights = &clusterData->clusterWeights;
+        if (!p_Clusters)
+            p_Clusters = std::make_shared<Matrix<precision, int_size>>(numClusters, dataCols(), false);
+        else if (p_Clusters.use_count() == 1)
+            p_Clusters->clear();
+
+        if (!p_Clustering)
+            p_Clustering = std::make_shared<std::vector<int_size>>(m_TotalNumData, -1);
+        else
+            std::fill(p_Clustering->begin(), p_Clustering->end(), -1);
+
+        if (!p_ClusterWeights)
+            p_ClusterWeights = std::make_shared<std::vector<precision>>(numClusters, 0.0);
+        else
+            std::fill(p_ClusterWeights->begin(), p_ClusterWeights->end(), 0.0);
+
+        if (!p_SqDistances)
+            p_SqDistances = std::make_shared<std::vector<precision>>(m_TotalNumData, sqDistsFillVal);
+        else
+            std::fill(p_SqDistances->begin(), p_SqDistances->end(), sqDistsFillVal);
     }
 
-    inline void setSqDistances(std::vector<precision>* const sqDistances) { p_SqDistances = sqDistances; }
+    inline void setClusters(std::shared_ptr<Matrix<precision, int_size>> clusters) { p_Clusters = clusters; }
+
+    inline void compareResults(std::shared_ptr<ClusterResults<precision, int_size>> clusterResults)
+    {
+        precision currError = std::accumulate(p_SqDistances->begin(), p_SqDistances->end(), 0.0);
+
+        if (clusterResults->error > currError || clusterResults->error < 0)
+        {
+            clusterResults->error          = currError;
+            clusterResults->clusters       = std::move(p_Clusters);
+            clusterResults->clustering     = std::move(p_Clustering);
+            clusterResults->clusterWeights = std::move(p_ClusterWeights);
+            clusterResults->sqDistances    = std::move(p_SqDistances);
+        }
+    }
 
     inline const int_size totalNumData() noexcept { return m_TotalNumData; }
 
     inline const int rank() noexcept { return m_Rank; }
+
+    inline const int numProcs() noexcept { return static_cast<int>(m_Lengths.size()); }
 
     inline const Matrix<precision, int_size>* data() const noexcept { return p_Data; }
 
     inline const precision* dataAt(const int_size& idx) const { return p_Data->at(idx); }
 
     inline const int_size dataSize() const noexcept { return p_Data->size(); }
+
+    inline const int_size dataCols() const noexcept { return p_Data->cols(); }
 
     inline const std::vector<precision>* weights() const noexcept { return p_Weights; }
 
@@ -75,13 +115,15 @@ public:
 
     inline const std::vector<int_size>& lengths() const noexcept { return m_Lengths; }
 
+    inline const int_size& lengthsAt(const int& rank) const { return m_Lengths.at(rank); }
+
     inline const int_size* lengthsData() const noexcept { return m_Lengths.data(); }
 
     inline const int_size& myDisplacement() const noexcept { return m_Displacement; }
 
     inline const int_size* displacementsData() const noexcept { return m_Displacements.data(); }
 
-    inline const std::vector<int_size>* clustering() const noexcept { return p_Clustering; }
+    inline const std::vector<int_size>* clustering() const { return p_Clustering.get(); }
 
     inline int_size& clusteringAt(const int_size& dataIdx) { return p_Clustering->at(m_Displacement + dataIdx); }
 
@@ -89,7 +131,7 @@ public:
 
     inline int_size* clusteringData() noexcept { return p_Clustering->data(); }
 
-    inline const std::vector<precision>* sqDistances() const noexcept { return p_SqDistances; }
+    inline const std::vector<precision>* sqDistances() const { return p_SqDistances.get(); }
 
     inline precision& sqDistancesAt(const int_size& dataIdx) { return p_SqDistances->at(m_Displacement + dataIdx); }
 
@@ -99,17 +141,15 @@ public:
 
     inline typename std::vector<precision>::iterator sqDistancesEnd() noexcept { return p_SqDistances->end(); }
 
-    inline const std::vector<precision>* clusterWeights() const noexcept { return p_ClusterWeights; }
+    inline const std::vector<precision>* clusterWeights() const { return p_ClusterWeights.get(); }
 
     inline precision& clusterWeightsAt(const int_size& clusterIdx) { return p_ClusterWeights->at(clusterIdx); }
 
     inline const precision* clusterWeightsData() const noexcept { return p_ClusterWeights->data(); }
 
-    inline Matrix<precision, int_size>* clusters() noexcept { return p_Clusters; }
+    inline Matrix<precision, int_size>* clusters() { return p_Clusters.get(); }
 
     inline const int_size clustersRows() const noexcept { return p_Clusters->rows(); }
-
-    inline const int_size clustersCols() const noexcept { return p_Clusters->cols(); }
 
     inline precision* clustersData() noexcept { return p_Clusters->data(); }
 
