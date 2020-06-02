881203416ea154db9c0d2ae015774954c4a51c63
Eric Dang <edang830@gmail.com>
2020-06-02 15:13:23 -0700

    Add convenience include header

---
+++ include/kmeans/utils/assignment_updaters.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <kmeans/utils/assignment_updaters/assignment_updater.hpp>
+#include <kmeans/utils/assignment_updaters/new_assignment_updater.hpp>
+#include <kmeans/utils/assignment_updaters/reassignment_updater.hpp>
