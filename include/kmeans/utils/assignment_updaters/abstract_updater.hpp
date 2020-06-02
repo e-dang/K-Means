7aac0b189f6d5169ec6bebfdc3b0223f86a7bb3b
Eric Dang <edang830@gmail.com>
2020-06-02 15:12:02 -0700

    Implement AbstractAssignmentUpdater

---
+++ include/kmeans/utils/assignment_updaters/abstract_updater.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <kmeans/types/parallelism.hpp>
+#include <matrix/matrix.hpp>
+#include <type_traits>
+
+namespace hpkmeans
+{
+template <typename T, class DistanceFunc>
+class AbstractAssignmentUpdater
+{
+protected:
+    AbstractAssignmentUpdater() : m_distanceFunc(DistanceFunc::instance()) {}
+
+    virtual ~AbstractAssignmentUpdater() = default;
+
+    void updateClosestCentroid(const int32_t dataIdx, const Matrix<T>* const data, const Matrix<T>* const centroids,
+                               std::vector<int32_t>* const assignments, std::vector<T>* const sqDistances) const
+    {
+        auto closestCentroid = this->findClosestCentroid(data->crowBegin(dataIdx), data->crowEnd(dataIdx), centroids);
+        assignments->at(dataIdx) = closestCentroid.idx;
+        sqDistances->at(dataIdx) = std::pow(closestCentroid.distance, 2);
+    }
+
+private:
+    struct ClosestCentroid
+    {
+        int32_t idx;
+        T distance;
+
+        ClosestCentroid() : idx(-1), distance(std::numeric_limits<T>::max()) {}
+
+        bool isGreaterThan(const T otherDist) const { return distance > otherDist; }
+
+        void set(const int32_t idx, const T distance)
+        {
+            this->idx      = idx;
+            this->distance = distance;
+        }
+    };
+
+    template <typename Iter>
+    ClosestCentroid findClosestCentroid(Iter begin, Iter end, const Matrix<T>* const centroids) const
+    {
+        ClosestCentroid closestCentroid;
+
+        for (int32_t i = 0; i < centroids->numRows(); ++i)
+        {
+            auto dist = m_distanceFunc(begin, end, centroids->crowBegin(i), centroids->crowEnd(i));
+            if (closestCentroid.isGreaterThan(dist))
+                closestCentroid.set(i, dist);
+        }
+
+        return closestCentroid;
+    }
+
+protected:
+    DistanceFunc m_distanceFunc;
+};
+}
