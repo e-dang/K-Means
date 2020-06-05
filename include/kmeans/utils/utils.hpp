c8d2a04061d0d161eed9d14d39bbe2c45ff53b2a
Eric Dang <edang830@gmail.com>
2020-06-04 18:27:43 -0700

    Implement MPI utils

--- include/kmeans/utils/utils.hpp
+++ include/kmeans/utils/utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <mpi.h>
+
 #include <boost/generator_iterator.hpp>
 #include <boost/random.hpp>
 #include <chrono>
@@ -16,10 +18,43 @@ inline int64_t getTime()
 
 inline double getRandFraction()
 {
-    thread_local static RNGType rng(getTime());
+    // thread_local static RNGType rng(getTime());
+    thread_local static RNGType rng(0);
     thread_local static boost::uniform_real<double> distr(0, 1);
     thread_local static boost::variate_generator<RNGType, boost::uniform_real<double>> gen(rng, distr);
 
     return gen();
+}
+
+inline int getCommRank()
+{
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    return rank;
+}
+
+inline int getCommSize()
+{
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    return size;
+}
+
+template <typename T>
+MPI_Datatype matchMPIType()
+{
+    MPI_Datatype dtype;
+    MPI_Type_match_size(MPI_TYPECLASS_REAL, sizeof(T), &dtype);
+    return dtype;
+}
+
+template <typename Iter>
+void print(Iter begin, Iter end)
+{
+    for (; begin != end; ++begin)
+    {
+        std::cout << *begin << " ";
+    }
+    std::cout << '\n' << std::flush;
 }
 }  // namespace hpkmeans
