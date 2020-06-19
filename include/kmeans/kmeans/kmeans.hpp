9b963c0aeab54957376879580c12a4175830812c
Eric Dang <edang830@gmail.com>
2020-06-19 16:19:57 -0700

    Implement wrapper KMeans which uses a KMeansImpl

--- include/kmeans/kmeans/kmeans.hpp
+++ include/kmeans/kmeans/kmeans.hpp
@@ -1,10 +1,7 @@
 #pragma once
 
-#include <kmeans/distances.hpp>
-#include <kmeans/initializers/initializers.hpp>
-#include <kmeans/maximizers/maximizers.hpp>
-#include <memory>
-#include <string>
+#include <kmeans/kmeans/coreset_kmeans.hpp>
+#include <kmeans/kmeans/kmeans_impl.hpp>
 
 namespace hpkmeans
 {
@@ -13,42 +10,24 @@ class KMeans
 {
 public:
     KMeans(const std::string& initializer, const std::string& maximizer) :
-        p_initializer(createInitializer<T, Level, DistanceFunc>(initializer)),
-        p_maximizer(createMaximizer<T, Level, DistanceFunc>(maximizer))
+        p_impl(std::make_unique<KMeansImpl<T, Level, DistanceFunc>>(initializer, maximizer))
     {
     }
 
-    virtual ~KMeans() = default;
-
-    const Clusters<T, Level>* const fit(const Matrix<T>* const data, const int32_t& numClusters, const int& numRepeats)
+    KMeans(const std::string& initializer, const std::string& maximizer, const int32_t sampleSize) :
+        p_impl(std::make_unique<CoresetKmeans<T, Level, DistanceFunc>>(initializer, maximizer, sampleSize))
     {
-        Clusters<T, Level> clusters(numClusters, data);
-
-        for (int i = 0; i < numRepeats; ++i)
-        {
-            p_initializer->initialize(data, &clusters);
-            p_maximizer->maximize(data, &clusters);
-            compareResults(clusters, m_bestClusters);
-            clusters.clear();
-        }
-
-        return getResults();
     }
 
-    const Clusters<T, Level>* const getResults() const { return &m_bestClusters; }
-
-protected:
-    void compareResults(const Clusters<T, Level>& candidateClusters, Clusters<T, Level>& bestClusters)
+    const Clusters<T, Level>* const fit(const Matrix<T>* const data, const int32_t& numClusters, const int& numRepeats,
+                                        const std::vector<T>* const weights = nullptr)
     {
-        if (candidateClusters < bestClusters)
-            bestClusters = candidateClusters;
+        return p_impl->fit(data, numClusters, numRepeats, weights);
     }
 
-protected:
-    Clusters<T, Level> m_bestClusters;
+    const Clusters<T, Level>* const getResults() const { return p_impl->getResults(); }
 
 private:
-    std::unique_ptr<IInitializer<T, Level>> p_initializer;
-    std::unique_ptr<IMaximizer<T, Level>> p_maximizer;
+    std::unique_ptr<KMeansImpl<T, Level, DistanceFunc>> p_impl;
 };
 }  // namespace hpkmeans
