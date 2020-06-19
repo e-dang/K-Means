b1e22b2796500bac2852e2f6dfd3a6820824049b
Eric Dang <edang830@gmail.com>
2020-06-18 20:05:15 -0700

    Implement constexpr boolean parallelism funcs

--- include/kmeans/utils/utils.hpp
+++ include/kmeans/utils/utils.hpp
@@ -5,6 +5,7 @@
 #include <boost/generator_iterator.hpp>
 #include <boost/random.hpp>
 #include <chrono>
+#include <type_traits>
 
 typedef boost::mt19937 RNGType;
 
@@ -18,8 +19,7 @@ inline int64_t getTime()
 
 inline double getRandFraction()
 {
-    // thread_local static RNGType rng(getTime());
-    thread_local static RNGType rng(0);
+    thread_local static RNGType rng(getTime());
     thread_local static boost::uniform_real<double> distr(0, 1);
     thread_local static boost::variate_generator<RNGType, boost::uniform_real<double>> gen(rng, distr);
 
@@ -41,11 +41,14 @@ inline int getCommSize()
 }
 
 template <typename T>
-MPI_Datatype matchMPIType()
+constexpr MPI_Datatype matchMPIType()
 {
-    MPI_Datatype dtype;
-    MPI_Type_match_size(MPI_TYPECLASS_REAL, sizeof(T), &dtype);
-    return dtype;
+    static_assert(std::is_floating_point_v<T>, "Template parameter T must be a floating point type!");
+
+    if constexpr (std::is_same_v<T, double>)
+        return MPI_DOUBLE;
+
+    return MPI_FLOAT;
 }
 
 template <typename Iter>
@@ -56,5 +59,76 @@ void print(Iter begin, Iter end)
         std::cout << *begin << " ";
     }
     std::cout << '\n' << std::flush;
+}
+
+constexpr bool isSharedMemory(Parallelism parallelism)
+{
+    return parallelism == Parallelism::Serial || parallelism == Parallelism::OMP;
+}
+
+constexpr bool isDistributed(Parallelism parallelism)
+{
+    return parallelism == Parallelism::MPI || parallelism == Parallelism::Hybrid;
+}
+
+constexpr bool isSingleThreaded(Parallelism parallelism)
+{
+    return parallelism == Parallelism::Serial || parallelism == Parallelism::MPI;
+}
+
+constexpr bool isMultiThreaded(Parallelism parallelism)
+{
+    return parallelism == Parallelism::OMP || parallelism == Parallelism::Hybrid;
+}
+
+template <Parallelism Level>
+constexpr Parallelism getConjugateParallelism()
+{
+    if constexpr (Level == Parallelism::Serial)
+        return Parallelism::MPI;
+    else if constexpr (Level == Parallelism::OMP)
+        return Parallelism::Hybrid;
+    else if constexpr (Level == Parallelism::MPI)
+        return Parallelism::Serial;
+    else
+        return Parallelism::OMP;
+}
+
+template <Parallelism Level, class Container>
+inline std::enable_if_t<isSingleThreaded(Level), typename Container::value_type> accumulate(
+  const Container* container, const typename Container::value_type& initVal = 0.0)
+{
+    return std::accumulate(container->cbegin(), container->cend(), initVal);
+}
+
+template <Parallelism Level, class Container>
+inline std::enable_if_t<Level == Parallelism::OMP, typename Container::value_type> accumulate(
+  const Container* container, const typename Container::value_type& initVal = 0.0)
+{
+    typename Container::value_type sum = 0.0;
+
+#pragma omp parallel for schedule(static), reduction(+ : sum)
+    for (int32_t i = 0; i < container->size(); ++i)
+    {
+        sum += container->at(i);
+    }
+
+    return sum;
+}
+
+template <Parallelism Level, class Container>
+inline std::enable_if_t<Level == Parallelism::Hybrid, typename Container::value_type> accumulate(
+  const Container* container, const typename Container::value_type& initVal = 0.0)
+{
+    typename Container::value_type sum = 0.0;
+
+#pragma omp parallel for schedule(static), reduction(+ : sum)
+    for (int32_t i = 0; i < container->viewSize(); ++i)
+    {
+        sum += container->at(i);
+    }
+
+    MPI_Allreduce(MPI_IN_PLACE, &sum, 1, matchMPIType<typename Container::value_type>(), MPI_SUM, MPI_COMM_WORLD);
+    return sum;
 }
 }  // namespace hpkmeans
