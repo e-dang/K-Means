0876d959f8d52b5ac765ccf1d8c9b596ea976fc6
Eric Dang <edang830@gmail.com>
2020-06-24 17:13:51 -0700

    Use defines to select methods and parallelism

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,42 @@
 #include <hpkmeans/kmeans.hpp>
 
 using namespace hpkmeans;
+
+constexpr bool strings_equal(char const* a, char const* b)
+{
+    return *a == *b && (*a == '\0' || strings_equal(a + 1, b + 1));
+}
+
+constexpr Parallelism getParallelism()
+{
+    if constexpr (strings_equal(PARALLELISM, "serial"))
+        return Parallelism::Serial;
+    else if constexpr (strings_equal(PARALLELISM, "omp"))
+        return Parallelism::OMP;
+    else if constexpr (strings_equal(PARALLELISM, "mpi"))
+        return Parallelism::MPI;
+    else
+        return Parallelism::Hybrid;
+}
+
 typedef double value_t;
-constexpr Parallelism parallelism = Parallelism::OMP;
-constexpr bool coreset            = true;
-constexpr int numData             = 200000;
-constexpr int dims                = 2;
-constexpr int numClusters         = 30;
+constexpr char lloydMethod[]      = METHOD;
+constexpr Parallelism parallelism = getParallelism();
+constexpr bool coreset            = CORESET;
+constexpr int numData             = 1000000;
+constexpr int dims                = 10;
+constexpr int numClusters         = 50;
 constexpr int repeats             = 10;
-constexpr int coresetRepeats      = 10;
+constexpr int coresetRepeats      = 1;
 constexpr int numIters            = 1;
 constexpr int sampleSize          = numData / 10;
 
 std::unique_ptr<KMeans<value_t, parallelism>> createKmeans()
 {
     if constexpr (coreset)
-        return std::make_unique<KMeans<value_t, parallelism>>(OPTKPP, OPTLLOYD, coresetRepeats, sampleSize);
+        return std::make_unique<KMeans<value_t, parallelism>>(OPTKPP, lloydMethod, coresetRepeats, sampleSize);
     else
-        return std::make_unique<KMeans<value_t, parallelism>>(OPTKPP, OPTLLOYD);
+        return std::make_unique<KMeans<value_t, parallelism>>(OPTKPP, lloydMethod);
 }
 
 void runSharedMemory(std::string& filepath)
@@ -32,14 +51,16 @@ void runSharedMemory(std::string& filepath)
 
     auto kmeans = createKmeans();
     const Clusters<value_t, parallelism>* results;
+    int64_t time;
     for (int i = 0; i < numIters; ++i)
     {
         boost::timer::auto_cpu_timer t;
         results = kmeans->fit(&data, numClusters, repeats);
+        time    = t.elapsed().wall;
     }
 
     std::cout << "Error: " << results->getError() << '\n';
-    writer.writeClusterResults(results, 0, filepath);
+    writer.writeClusterResults(results, time, filepath);
 }
 
 void runDistributed(std::string& filepath)
@@ -57,16 +78,18 @@ void runDistributed(std::string& filepath)
 
     auto kmeans = createKmeans();
     const Clusters<value_t, parallelism>* results;
+    int64_t time;
     for (int i = 0; i < numIters; ++i)
     {
         boost::timer::auto_cpu_timer t;
         results = kmeans->fit(&data, numClusters, repeats);
+        time    = t.elapsed().wall;
     }
 
     if (rank == 0)
     {
         std::cout << "Error: " << results->getError() << '\n';
-        writer.writeClusterResults(results, 0, filepath);
+        writer.writeClusterResults(results, time, filepath);
     }
 
     MPI_Finalize();
@@ -74,10 +97,11 @@ void runDistributed(std::string& filepath)
 
 int main(int argc, char* argv[])
 {
-    std::string filepath = /*INSERT FILEPATH HERE*/ std::to_string(numData) + "_" + std::to_string(dims) + ".txt";
+    std::string filepath = /*INSERT FILEPATH HERE*/ std::to_string(numData) + "_" + std::to_string(dims) + "_" +
+                           std::to_string(numClusters) + ".txt";
 
-    std::cout << "Method: " << KPP << "\nParallelism: " << parallelismToString(parallelism) << "\nData: " << filepath
-              << '\n';
+    std::cout << "Method: " << OPTKPP << "\nParallelism: " << parallelismToString(parallelism)
+              << "\nCoreset: " << coreset << "\nData: " << filepath << '\n';
 
     if (isSharedMemory(parallelism))
         runSharedMemory(filepath);
